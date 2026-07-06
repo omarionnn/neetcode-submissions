b7e71562dfd8a877f97544f1e0a6067d13374276
Omari Nyarko <75476241+omarionnn@users.noreply.github.com>
2026-07-06 09:50:56 -0400

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = []
+        comb = []
+        candidates.sort()
+
+
+        def backtrack(i, sum):
+            if sum == target:
+                output.append(comb.copy())
+                return 
+
+            if target < sum or i >= len(candidates):
+                return 
+
+            comb.append(candidates[i])
+            backtrack(i + 1, sum + candidates[i])
+
+            comb.pop()
+
+            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+                i += 1
+            backtrack(i + 1, sum)
+
+
+        backtrack(0, 0)
+        return output
+        
