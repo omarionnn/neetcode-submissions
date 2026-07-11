8e6ca2d5153f0b9d19e7f80a7f29478bfdcad9e5
Omari Nyarko <75476241+omarionnn@users.noreply.github.com>
2026-07-11 08:22:12 -0400

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+
+        def backtrack(open, close, arr):
+            if open == close == n:
+                output.append(''.join(arr))
+                return 
+
+            if open < n:
+                arr.append('(')
+                backtrack(open + 1, close, arr)
+                arr.pop()
+
+            if close < open:
+                arr.append(')')
+                backtrack(open, close + 1, arr)
+                arr.pop()
+
+
+        backtrack(0, 0, [])
+        return output        
